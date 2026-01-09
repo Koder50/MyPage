4c41866de00095f0d3aaa51fa7456296782559a9
Koder50 <kgoralczyk@gmail.com>
2026-01-09 16:05:59 +0100

    Add Google Analitycs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare function gtag(...args: any[]): void;
 
 @Component({
   selector: 'pierwszastrona',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Karol Góralczyk - strona';
+
+  constructor(private router: Router) {}
   
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        gtag('config', 'G-JWMF2ZZY2Z', { page_path: event.urlAfterRedirects });
+      });
+  }
 }
